d4bbfe54116966eb08d64ed0b8cde3a50159bd49
Eugene <eugenefung79@gmail.com>
2023-04-15 14:02:28 -0400

    add index view and partials

--- controllers/flights.js
+++ controllers/flights.js
@@ -14,8 +14,15 @@ function index(req, res) {
   })
 }
 
+function newFlight(req, res) {
+  res.render('flights/new', {
+    title: 'Add Flight'
+  })
+}
+
 
 
 export{
   index,
+  newFlight as new,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,5 +4,6 @@ import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
 router.get('/', flightsCtrl.index)
+router.get('/new', flightsCtrl.new)
 
 export { router }
